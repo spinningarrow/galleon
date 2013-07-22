0f3c45a882461a6d1cc4137f1e5b34489a157d4b
Sahil Bajaj <sahil29@gmail.com>
2013-07-22 21:13:57 +0530

    Add Galleon files
    * Added files based on previous galleon version with ideas from Meteor's
      Parties example
    * Remember to symlink .meteor to ~ (~/.galleon/.meteor) because of vagrant

---
+++ model.js
@@ -0,0 +1,44 @@
+Expenditures = new Meteor.Collection("expenditures");
+
+
+/*
+user: user id (owner)
+date: datetime
+value: float
+currency: string (3 letter code)
+tags: array of strings
+*/
+
+// Expenditures.allow({})
+
+Meteor.methods({
+
+	// options should include: date, value, currency, tags
+	createExpenditure: function (options) {
+		options = options || {};
+
+		// Validation stuffs
+		/*if (! (typeof options.title === "string" && options.title.length &&
+					typeof options.description === "string" &&
+					options.description.length &&
+					typeof options.x === "number" && options.x >= 0 && options.x <= 1 &&
+					typeof options.y === "number" && options.y >= 0 && options.y <= 1))
+			throw new Meteor.Error(400, "Required parameter missing");
+		if (options.title.length > 100)
+			throw new Meteor.Error(413, "Title too long");
+		if (options.description.length > 1000)
+			throw new Meteor.Error(413, "Description too long");*/
+
+		if (!this.userId) {
+			throw new Meteor.Error(403, "You must be logged in.")
+		}
+
+		return Expenditures.insert({
+			user: this.userId,
+			date: options.date,
+			value: options.value,
+			currency: options.currency,
+			tags: options.tags
+		})
+	}
+})
