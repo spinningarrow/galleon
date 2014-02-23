6c55fbc419266a0fedae00133af61a121d07e6d4
Sahil Bajaj <sahil29@gmail.com>
2014-02-23 16:34:25 +0800

    Use Iron Router
    * Added Iron Router package (using Meteorite)
    * Added routes for '/' and '/details'
    * Modified templates to work with router
    * Removed index template since it is no longer needed

--- client/client.js
+++ client/client.js
@@ -1,1 +1,17 @@
 Meteor.subscribe('expenditures')
+
+// Routing
+Router.configure({
+	layoutTemplate: 'layout'
+})
+
+Router.map(function () {
+	this.route('home', {
+		path: '/',
+		template: 'create'
+	})
+
+	this.route('details', {
+		path: '/details'
+	})
+})
